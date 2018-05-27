5ebca4ff02b0de27ddc8fb2c9f5c7cdd850699e3
celitedev <iagilelite@gmail.com>
2018-05-27 21:54:13 +0200

    update Header component

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import Link from 'components/Link';
 import cx from 'classnames';
 import type { Map } from 'immutable';
+import currencyFormatter from 'currency-formatter';
 
 import Button from 'components/Button';
 
@@ -11,6 +12,7 @@ import './styles.scss';
 
 type Props = {
   user: Object,
+  balance: number,
   logout: Function,
   replace: Function,
   pathname: string,
@@ -23,20 +25,26 @@ class Header extends React.Component<Props> {
     }
   }
   render() {
-    const { user, logout, replace, pathname } = this.props;
+    const { user, balance, logout, replace, pathname } = this.props;
     const className = cx('header container');
     const authRoute =
       pathname.includes('login') || pathname.includes('register');
     return (
       <div className={className}>
         <div className="header__topLine row align-middle">
-          <div className="small-order-2 shrink column">
-            <h1>MyCrypto.com</h1>
+          <div className="small-order-2 medium-shrink column small-12">
+            <Link className="header__title" to="/">
+              <h1>MyCrypto.com</h1>
+            </Link>
           </div>
           {!authRoute && (
             <div className="small-order-3 medium-order-4 column text-right">
               {user ? (
                 <div>
+                  <Link to="/transactions">Transactions</Link>&nbsp;&nbsp;
+                  <Link>{`Balance(${currencyFormatter.format(balance, {
+                    code: 'USD',
+                  })}$)`}</Link>&nbsp;&nbsp;
                   <Link onClick={() => logout()}>Log out</Link>
                 </div>
               ) : (
